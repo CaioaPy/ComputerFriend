9e4af1f483fbeba5c0edbff4907e4097d805d824
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-02-19 08:31:20 -0300

    Small text fixes

--- src/diagram.py
+++ src/diagram.py
@@ -1,7 +1,7 @@
 # this file gonna have most of the thing for now, like functions and ideas. as i learn more i'm gonna separate the things in a very better way :)
 def home():
-    action = input("What do you want to do?")
+  action = input("What do you want to do, " + username + "?")
 username = input("Hello, how would you like me to call you?")
 
-print("Hello " + username + ", welcome to Computer Friend\n as you can imagine I'm a friend inside your computer")
+print("Hello " + username + ", welcome to Computer Friend\nas you can imagine I'm a friend inside your computer")
 home()
