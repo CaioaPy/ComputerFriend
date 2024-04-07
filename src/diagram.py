e55e6ccb9c77a9cc7147076bf682f6a9c931fd25
yatomikazuchi <caioevilabner@gmail.com>
2024-04-06 23:24:07 -0300

    updated

--- src/diagram.py
+++ src/diagram.py
@@ -1,7 +1,16 @@
 # this file gonna have most of the thing for now, like functions and ideas. as i learn more i'm gonna separate the things in a very better way :)
+from tkinter import *
 def home():
     action = input("What do you want to do?")
 username = input("Hello, how would you like me to call you?")
 
 print("Hello " + username + ", welcome to Computer Friend\n as you can imagine I'm a friend inside your computer")
-home()
+
+root = Tk()
+
+# create
+l1 = Label(root, text="home\n \n\n\nhello")
+# then putting it on screen
+l1.pack()
+
+root.mainloop()
