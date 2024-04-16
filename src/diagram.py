a717499b20a444e5c14a4dba71f28675c403aa4b
yatomikazuchi <caioevilabner@gmail.com>
2024-04-16 20:50:17 -0300

    updated

--- src/diagram.py
+++ src/diagram.py
@@ -6,7 +6,10 @@ root.title("Computer Friend")
 
 e = Entry(root, width=40, borderwidth=3)
 
-username = input("Hello, how would you like me to call you?")
+usernamen = 0
+while usernamen == 0:
+  username = input("Hello, how would you like me to call you?")
+  usernamen + 1
 
 print("Hello " + username + ", welcome to Computer Friend\nas you can imagine I'm a friend inside your computer")
 
