0da32c260f6b977cb542a6e3f21f3005031ae90e
yatomikazuchi <caioevilabner@gmail.com>
2024-04-15 23:35:24 -0300

    updated

--- src/diagram.py
+++ src/diagram.py
@@ -1,13 +1,12 @@
 # this file gonna have most of the thing for now, like functions and ideas. as i learn more i'm gonna separate the things in a very better way :)
 from tkinter import *
-def home():
-  action = input("What do you want to do, " + username + "?")
-username = input("Hello, how would you like me to call you?")
-
-print("Hello " + username + ", welcome to Computer Friend\nas you can imagine I'm a friend inside your computer")
 
 root = Tk()
 root.title("Computer Friend")
+
+username = input("Hello, how would you like me to call you?")
+
+print("Hello " + username + ", welcome to Computer Friend\nas you can imagine I'm a friend inside your computer")
 
 #labels
 l1 = Label(root, text="Welcome!")
@@ -22,3 +21,7 @@ placeholder.grid(row=1, column=0)
 helloButton.grid(row=2, column=1, padx=30, pady=15)
 
 root.mainloop()
+
+#functions
+def home():
+  action = input("What do you want to do, " + username + "?")
