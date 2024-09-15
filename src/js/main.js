d125b7b052c2457e2b3fdacf6b81a8e0c4c6fc0b
yatomikazuchi <caioevilabner@gmail.com>
2024-09-15 20:43:17 -0300

    updated

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,22 @@ var sendBtn = document.getElementById('sendBtn');
 var textbox = document.getElementById('usertext');
 var chat_container = document.getElementById('chat_container');
 
+chatBotMessage("heyooo")
+
+function chatBotMessage(messageText){
+    var messageElement = document.createElement('div');
+    messageElement.classList.add('w-50');
+    messageElement.classList.add('float-left');
+    messageElement.classList.add('shadown-sm');
+    messageElement.style.margin="10px";
+    messageElement.style.padding="5px";
+
+    messageElement.innerHTML = "<span>Computer Friend: </span>" +
+    "<span style="+"margin-top:10px; padding:5px"+">"+ messageText +"</span>";
+    
+    chat_container.appendChild(messageElement);
+}
+
 function sendMessage(messageText){
     var messageElement = document.createElement('div');
     messageElement.classList.add('w-50');
