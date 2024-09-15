d70599199cfe672307c3029cf79b41c04665589d
yatomikazuchi <caioevilabner@gmail.com>
2024-09-15 20:40:16 -0300

    user text done

--- src/js/main.js
+++ src/js/main.js
@@ -4,19 +4,19 @@ var chat_container = document.getElementById('chat_container');
 
 function sendMessage(messageText){
     var messageElement = document.createElement('div');
-    messageElement.classlist.add('w-50');
-    messageElement.classlist.add('float-left');
-    messageElement.classlist.add('shadown-sm');
-    messageElement.style.margin="10px"
-    messageElement.style.padding="5px"
+    messageElement.classList.add('w-50');
+    messageElement.classList.add('float-left');
+    messageElement.classList.add('shadown-sm');
+    messageElement.style.margin="10px";
+    messageElement.style.padding="5px";
 
-    messageElement.innerHTML = "<span>Me:</span>" +
-                "<span style="+"margin-top:10px; padding:5px">+ messageText +"</span>";
+    messageElement.innerHTML = "<span>Me: </span>" +
+    "<span style="+"margin-top:10px; padding:5px"+">"+ messageText +"</span>";
+    
     chat_container.appendChild(messageElement);
 }
 
 sendBtn.addEventListener('click', function(e){
     let messageText = textbox.value;
-    sendMessage(messageText)
-
+    sendMessage(messageText);
 });
