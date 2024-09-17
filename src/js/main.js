79054680bee1efc630a6c367d720711c114f4eb2
yatomikazuchi <caioevilabner@gmail.com>
2024-09-16 22:43:36 -0300

    responses holder (no API)

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,13 @@
 var sendBtn = document.getElementById('sendBtn');
 var textbox = document.getElementById('usertext');
 var chat_container = document.getElementById('chat_container');
+
 var user = {message:""};
+var arrayOfMessages = [
+    {"message":"how are you?", "response":"im fine"},
+    {"message":"hi", "response":"heyoo!"},
+    {"message":"hello", "response":"heloo :3"}
+];
 
 sendChatBotMessage("heyooo");
 
